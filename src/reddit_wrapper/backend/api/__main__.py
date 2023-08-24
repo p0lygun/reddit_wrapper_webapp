0cc82c5bc97c330b802c4371b3736e206f579776
p0lygun <solankivibhakar82@gmail.com>
2023-08-24 19:57:11 +0530

    feat(backend): add `lifespan` function to manage aiohttp session

--- src/reddit_wrapper/backend/api/__main__.py
+++ src/reddit_wrapper/backend/api/__main__.py
@@ -1,6 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from ..session_manager import Manager
 from .routers import reddit
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(*args, **kwargs):
+    """Context manager for the lifespan of the application"""
+    Manager()
+    yield
+    await Manager.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(reddit.reddit_router)
